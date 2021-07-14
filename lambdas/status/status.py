1caa4e8ad36ac1054cd6b7829d7e06ebf5551026
Philipp Sonntag <psonntag@godaddy.com>
2021-07-14 18:50:45 +0200

    Update status.py
    Typos

--- lambdas/status/status.py
+++ lambdas/status/status.py
@@ -109,10 +109,10 @@ HTML_FOOTER = dedent(
     </p>
     <h2>Metrics</h2>
     <p>
-    Please note that the <i>Clicked Link</i> and <i>Email Opened</i> metric reflect
+    Please note that the <i>Clicked Link</i> and <i>Email Opened</i> metrics reflect
     the number of emails sent due to Security tooling that automatically evaluates
     links in emails; this does not reflect the actual count of clicked links or opened
-    emails. We are working towards addressing those metrices.
+    emails. We are working towards addressing those metrics.
     </p>
     </body>
     </html>
